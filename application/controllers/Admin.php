edb9f92d0333b0e9d173c30d4c8d2205ecce144e
akunbeben <akunbeben@gmail.com>
2020-09-05 19:01:49 +0800

    update resources

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -31,8 +31,6 @@ class Admin extends CI_Controller
   {
     $data = $this->Report->getDataOmset();
 
-    // echo $this->db->last_query();
-
     header('Content-Type: application/json');
     echo json_encode(['status' => true, 'data' => $data]);
   }
@@ -48,8 +46,6 @@ class Admin extends CI_Controller
   public function api_profit()
   {
     $data = $this->Report->getDataProfit();
-
-    // echo $this->db->last_query();
 
     header('Content-Type: application/json');
     echo json_encode(['status' => true, 'data' => $data]);
